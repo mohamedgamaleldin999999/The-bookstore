f0009d7be140caa7f25c14910a3167a21a560e74
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-23 17:25:46 +0300

    more imports

--- src/BookPage.js
+++ src/BookPage.js
@@ -5,6 +5,7 @@ import Percentage from './components/BookComponent/Percentage';
 import style from './BookPage.module.css';
 import EachBook from './components/BookComponent/eachBook';
 import AddBook from './components/BookComponent/AddBook';
+import { booksFromAPI } from './redux/books/books';
 
 function BookPage() {
   const books = useSelector((state) => state.Books, shallowEqual);
