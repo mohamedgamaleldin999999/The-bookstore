5daa28991dfc9bc01538a7c23efb3a879ff7f492
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-23 04:23:14 +0300

    Update bookslice

--- src/redux/books/bookslice.js
+++ src/redux/books/bookslice.js
@@ -4,25 +4,47 @@ const Actions = [
   { ADD: 'bookstore/book/ADD' },
   { REMOVE: 'bookstore/book/REMOVE' },
 ];
+// state initialized as array
+
+const initialState = [
+  {
+    id: uuidv4(),
+    title: 'The First Game',
+    author: 'Suarez',
+    topic: 'Actions',
+    category: 'Economy',
+  },
+  {
+    id: uuidv4(),
+    title: 'The Second Science',
+    author: 'Daniel',
+    topic: 'Actions',
+    category: 'Science Fiction',
+  },
+  {
+    id: uuidv4(),
+    title: 'The Third Play',
+    author: 'Jimmy',
+    topic: 'Actions',
+    category: 'Action',
+  },
+];
 
 // Reducer
-// state initialized as array
-const reducer = (state = [], action) => {
+const reducer = (state = initialState, action) => {
   const { payLoad } = action;
   switch (action.type) {
     case Actions[0].ADD:
       return [...state, payLoad.book];
 
     case Actions[1].REMOVE:
-      state.filter((book) => book.id !== payLoad.id);
+      return state.filter((book) => book.id !== payLoad.id);
 
     default:
       return state;
   }
 };
-
 // The Action   Creators
 export const AddBook = (book) => ({ type: Actions[0].ADD, payLoad: { book } });
 export const RemoveBook = (id) => ({ type: Actions[1].REMOVE, payLoad: { id } });
-
 export default reducer;
