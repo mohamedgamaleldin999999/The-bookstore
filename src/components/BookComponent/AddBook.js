beca3dfcba72b5a011fb20646f72d01ddfa9db89
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-24 02:08:02 +0300

    import AddBook.module.css

--- src/components/BookComponent/AddBook.js
+++ src/components/BookComponent/AddBook.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../../redux/books/bookslice';
+import styles from './AddBook.module.css';
 
 function AddBookForm() {
   const dispatch = useDispatch();
