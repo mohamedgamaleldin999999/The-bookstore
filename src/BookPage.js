565121de42ad6d429743fc610a4cb8a038db5883
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-23 17:24:27 +0300

    Add imports

--- src/BookPage.js
+++ src/BookPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import Nav from './components/Nav';
 import Percentage from './components/BookComponent/Percentage';
 import style from './BookPage.module.css';
