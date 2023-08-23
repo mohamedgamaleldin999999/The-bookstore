64ec5151578559cb09b27b5d5ad7481eed650421
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-23 04:18:09 +0300

    Add percentage component

---
+++ src/components/BookComponent/Percentage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Percentage() {
+  return (
+    <div>
+      <div />
+      <div>
+        <span>
+          60%
+        </span>
+        <span>
+          Completed
+        </span>
+      </div>
+      <div>grey line</div>
+      <div>
+        <h2>CURRENT CHAPTER</h2>
+        <h3>chapter</h3>
+        <button type="button">Update Progress</button>
+      </div>
+    </div>
+  );
+}
+export default Percentage;
